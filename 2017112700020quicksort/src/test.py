28e228adb9b4877f0806002231f2907f13bf4ac7
Administrator <Administrator@LPLHUCTKWMGMPAP>
2017-11-27 11:05:30 +0800

    my first push to git

---
+++ 2017112700020quicksort/src/test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Function:
+【记录】使用Python的IDE：Eclipse+PyDev
+ 
+http://www.crifan.com/try_with_python_ide_eclipse_pydev
+ 
+Author:     Crifan Li
+Version:    2012-12-29
+Contact:    admin at crifan dot com
+"""
+ 
+import platform
+ 
+print( "Current platform.uname() in Ecplise+PyDev=",platform.uname())
+print("Hello World!")
